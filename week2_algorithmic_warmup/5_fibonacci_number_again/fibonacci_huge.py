20e7df65c6a3be9e15dc88e00dd0475a45fd4b5a
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-09-11 10:46:26 +0600

    Fibonacci Huge solution

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -13,37 +13,33 @@ def get_fibonacci_huge_naive(n, m):
 
     return current % m
 
-def calc_fib_optimized(n):
+def get_fibonacci_huge_optimized(n, m):
+    # calculate fn % m and append to period list
+    # if half of period is equal to another half, then stop counting fn
+    # l = len(period) // 2
+    # calculate reminder = n % l
+    # get element period[reminder]
     if n <= 1:
-        return n
+        return n % m
     
+    period = [0, 1]
+    counter = 2
     previous = 0
     current = 1
 
     for i in range(n-1):
-        previous, current = current, previous + current
+        previous, current = current, (previous + current) % m
+        period.append(current % m)
+        counter += 1
+        half = counter // 2
 
-    return current
-
-def get_fibonacci_huge_optimized(n, m):
-    # calculate fn % m
-    # append value it to period
-    # if half of period is equal to another half, then stop counting fn
-    # get reminder n % m = l
-    # get f(l) % m
-    period = []
-    period_len = len(period)
-
-    for i in range(n):
-        period.append(calc_fib_optimized(i) % m)
-        period_len = len(period)
-        
-        if period[:period_len] == period[period_len]:
+        if period[:half] == period[half:]:
+            period = period[:half]
             break
-    
-    l = n % period_len
 
-    return calc_fib_optimized(l) % m
+    reminder = n % len(period)
+    
+    return period[reminder]
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
